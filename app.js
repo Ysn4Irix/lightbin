22eb4604b42df55f0870e9aa7792f3caa47ce3c7
Ysn4Irix <80527916+Ysn4Irix@users.noreply.github.com>
2023-05-24 22:05:36 +0100

    :sparkles: adding api endpoint

--- app.js
+++ app.js
@@ -2,49 +2,50 @@
  * @author Ysn4Irix
  * @email ysn4irix@gmail.com
  * @create date 10-10-2021
- * @modify date 11-10-2021
+ * @modify date 24-05-2023
  * @desc [Application entry point]
  */
 
-require("dotenv").config();
-const express = require("express");
-const path = require("path");
-const logger = require("morgan");
-const helmet = require("helmet");
-const mongoose = require("mongoose");
+require('dotenv').config()
+const express = require('express')
+const path = require('path')
+const logger = require('morgan')
+const helmet = require('helmet')
+const mongoose = require('mongoose')
 
-const router = require("./routes/docs");
-const middlewares = require("./helpers/middlewares");
-const app = express();
+const docsRouter = require('./routes/docs')
+const apiRouter = require('./routes/api')
+const middlewares = require('./helpers/middlewares')
+const app = express()
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use(logger("common"));
-app.use(helmet());
+app.use(logger('common'))
+app.use(helmet())
 app.use(
   helmet.contentSecurityPolicy({
     useDefaults: true,
     directives: {
-      "script-src": [
+      'script-src': [
         "'self' unpkg.com cdnjs.cloudflare.com",
         "'unsafe-eval'",
         "'unsafe-inline'",
       ],
-      "font-src": [
+      'font-src': [
         "'self' fonts.googleapis.com fonts.gstatic.com cdnjs.cloudflare.com",
       ],
-      "style-src": ["'self' fonts.googleapis.com cdnjs.cloudflare.com"],
+      'style-src': ["'self' fonts.googleapis.com cdnjs.cloudflare.com"],
     },
   })
-);
+)
 
 app.use(
   express.urlencoded({
     extended: true,
   })
-);
+)
 
 mongoose
   .connect(process.env.MONGODB_URI, {
@@ -52,20 +53,21 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log("Connected To Database");
+    console.log('Connected To Database')
   })
   .catch((err) => {
-    console.log("Not Connected " + err);
-  });
+    console.log('Not Connected ' + err)
+  })
 
-app.use("/", router);
+app.use('/', docsRouter)
+app.use('/', apiRouter)
 
-app.use(middlewares.notFound);
-app.use(middlewares.errorHandler);
+app.use(middlewares.notFound)
+app.use(middlewares.errorHandler)
 
-const port = process.env.PORT || 2222;
+const port = process.env.PORT || 2222
 app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
-});
+  console.log(`Listening at http://localhost:${port}`)
+})
 
-module.exports = app;
+module.exports = app
